76bf04ebc82e92b121641768a7773b7d534090e1
AAnzel <44969003+AAnzel@users.noreply.github.com>
2023-01-23 18:21:12 +0100

    Added: Started writing unit tests.

--- Source/test.py
+++ Source/test.py
@@ -1,4 +1,6 @@
 import os
+import unittest
+
 import numpy as np
 import pandas as pd
 from plotly.subplots import make_subplots
@@ -23,28 +25,24 @@ path_root_data = os.path.join('..', 'Data')
 path_root_data_results = os.path.join(path_root_data, 'Results')
 
 INT_RANDOM_SEED = 42
+STRING_REFERENCE_MODEL = 'Model 0'
 np.random.seed(INT_RANDOM_SEED)
 
 
-def df_generate_new_timepoint(df_input):
+def df_generate():
     """
-    df_generate_new_timepoint adds noise to each column of the input dataframe,
-    thus creating a slightly modified version of that dataframe. This version
-    can be considered a snapshot of a phenomenon in a different time point.
-
-    Args:
-        pd.DataFrame(): An input dataframe that contains models in columns.
+    df_generate_new_timepoint creates a new DataFrame for testing purposes.
 
     Returns:
-        pd.DataFrame(): An output dataframe which comes from the procedure
-        described above.
+        pandas.DataFrame(): An output DataFrame.
     """
 
     df_result = pd.DataFrame()
-    int_len = df_input.shape[0]
-    for string_one_column in df_input.columns:
-        df_result[string_one_column] = df_input[
-            string_one_column].to_numpy() + np.random.normal(0, 1, int_len)
+    int_row_len = 100
+    int_column_len = 10
+    for int_num_model in range(int_column_len):
+        df_result['Model ' + str(int_num_model)] = np.random.normal(
+            0, 1, int_row_len)
 
     return df_result
 
@@ -77,10 +75,7 @@ def chart_scatter_anscombes_quartet(df_input):
     return chart_result
 
 
-def main():
-    if not os.path.exists(path_root_data_results):
-        os.mkdir(path_root_data_results)
-
+def test_output():
     dict_mi_parameters_features_continous_target_continous = dict(
         string_entropy_method='auto',
         int_mi_n_neighbors=3,
@@ -122,5 +117,34 @@ def main():
     return None
 
 
+class TestDiagrams(unittest.TestCase):
+    def test_df_calculate_td_properties(self):
+        self.assertIsInstance(
+            diag.df_calculate_td_properties(
+                df_generate(), string_reference_model=STRING_REFERENCE_MODEL),
+            pd.DataFrame)
+
+    def test_df_calculate_mid_properties(self):
+        self.assertIsInstance(
+            diag.df_calculate_mid_properties(
+                df_generate(), string_reference_model=STRING_REFERENCE_MODEL),
+            pd.DataFrame)
+
+    def test_df_calculate_all_properties(self):
+        self.assertIsInstance(
+            diag.df_calculate_all_properties(
+                df_generate(), string_reference_model=STRING_REFERENCE_MODEL),
+            pd.DataFrame)
+
+    def test_chart_create_taylor_diagram(self):
+        return None
+
+    def test_chart_create_mi_diagram(self):
+        return None
+
+    def test_chart_create_all_diagrams(self):
+        return None
+
+
 if __name__ == '__main__':
-    main()
+    unittest.main()
